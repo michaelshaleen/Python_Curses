c99cdc09ab159a8f064844d033fc5bd3571489f8
michaelshaleen <shaleenmichael@yahoo.com>
2022-01-16 13:03:06 -0600

    curses color pair

--- cp.py
+++ cp.py
@@ -2,10 +2,17 @@ import curses
 from curses import wrapper
 
 def main(stdscr):
+  # 1 is ID, referenced later by stdscr
+  curses.init_pair(1, curses.COLOR_MAGENTA, curses.COLOR_YELLOW)
+  MAGENTA_AND_YELLOW = curses.color_pair(1)
   stdscr.clear()
   # row and column
-  stdscr.addstr(15, 40,"Hello planet", curses.A_BOLD)
+  # A_ == attributes
+  # coordinates, string, curses attritbute
+  stdscr.addstr(15, 40,"Hello planet", MAGENTA_AND_YELLOW)
   stdscr.addstr(5, 5,"Solar System", curses.A_UNDERLINE)
+  stdscr.addstr(5, 5,"Solar System", curses.A_UNDERLINE)
+
 
 # screen over terminal so print() wont work
 
