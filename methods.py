5e3a8ca925e56ec1abff4745e3bebe692c852b0d
michaelshaleen <shaleenmichael@yahoo.com>
2022-01-29 09:37:06 -0600

    curses echo shows user input on screen

--- methods.py
+++ methods.py
@@ -9,14 +9,33 @@ def main(stdscr):
   curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
   MAGENTA_AND_YELLOW = curses.color_pair(1)
   GREEN_AND_BLACK = curses.color_pair(2)
+  # echo keystrokes that user types
+  curses.echo()
+  
 
+  # border colors
+  stdscr.attron(MAGENTA_AND_YELLOW)
+  stdscr.border()
+  stdscr.attroff(MAGENTA_AND_YELLOW)
+
+
+
+
+  # attribute on offs
   stdscr.attron(GREEN_AND_BLACK)
   rectangle(stdscr, 1, 1, 5, 20)
   stdscr.attroff(GREEN_AND_BLACK)
+  stdscr.addstr(5, 30, "Hello Mars")
+# moves cursor
+  stdscr.move(10, 20)
 
   stdscr.refresh()
 
-  stdscr.getch()
+
+  while True:
+    key = stdscr.getkey()
+    if key == "q":
+      break
 
   
 
